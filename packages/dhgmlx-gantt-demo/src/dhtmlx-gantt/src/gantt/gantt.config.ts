3e89470ce4cf34e9087580050219f5ad8aba397c
青衫依旧 <2461521018@qq.com>
2023-12-20 17:45:25 +0800

    feat: theme config

--- packages/dhgmlx-gantt-demo/src/dhtmlx-gantt/src/gantt/gantt.config.ts
+++ packages/dhgmlx-gantt-demo/src/dhtmlx-gantt/src/gantt/gantt.config.ts
@@ -1,5 +1,8 @@
 import { gantt } from "dhtmlx-gantt";
 import "dhtmlx-gantt/codebase/dhtmlxgantt.css";
+import "dhtmlx-gantt/codebase/skins/dhtmlxgantt_material.css";
+
+
 
 gantt.config.date_format = "%Y-%m-%d";
 gantt.i18n.setLocale("cn");
